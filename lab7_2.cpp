3ea6aa443a0714c566d86fe2bdcca080ab80f259
tensept <vankaew58@gmail.com>
2022-12-13 16:19:36 +0700

    lab7_2 compleated

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -5,9 +5,26 @@ using namespace std;
 int main(){
 	
 	int i = 0, P[5000], k, result;
+	cout << "Please input k: ";
+	cin >> k;
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
+		}
+	if(k>0){
+		i = 0, result = 0;
+		while(i<5000){
+				if(P[i]%k==0){
+					result = result - P[i];
+				}
+				else{
+					result = result + P[i];
+				}
+				i = i+1;
+			}
+				cout << "Result = " << result;
+	}else{
+		cout << "Invalid input!!!";
 	}
 	
 	return 0;
