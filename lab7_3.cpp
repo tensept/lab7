53c91b14a76ff63c9646c5f09a275693d8f33a5c
tensept <vankaew58@gmail.com>
2022-12-13 16:59:12 +0700

    lab7_3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,22 @@
 
 using namespace std;
 
+int adiff(int a, int b){
+ int c = abs(a-b)%360;
+ if(c>180){
+  c = 360-c;
+ }
+  return c;
+}
+
 
 int main(){
-  cout << adiff(180,270);
+  /*cout << adiff(180,270);
   cout << adiff(210,45);
   cout << adiff(0,360);
   cout << adiff(10,350);
-  cout << adiff(95,260);
+  cout << adiff(95,260);*/
   cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  //cout << adiff(1000,280);
+  //cout << adiff(60,244);
 }
